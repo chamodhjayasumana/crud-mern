1eb02adca70124d06715bf77214c62c57de573df
Chamodh Jayasumana <chamodhinduwarajayasumana@gmail.com>
2024-07-21 11:26:42 +0530

    create API section

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 const app =express()
 app.use(cors())
+app.use(express.json())
 
 const port =process.env.port|| 8080
 
@@ -16,4 +17,22 @@ const schemaData = mongoose.Schema({
     timestamps : true
 })
 
-mongoose.connect("mongodb://127.0.0.1:27017/crudoperation")
+const userModel = mongoose.model("user",schemaData)
+
+//API section 
+// read 
+//"http://localhost:8080/"
+app.get("/",async(req,res)=>{
+    const data = await userModel.find({})
+        res.json({success : true , data : data})
+    })
+
+
+    mongoose.connect("mongodb://127.0.0.1:27017/crudoperation")
+
+    .then(()=>{
+        console.log("connect to DB")
+        app.listen(port,()=>console.log("Server is running"))
+    
+    })
+    .catch((err)=>console.log(err))
